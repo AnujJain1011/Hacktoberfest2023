57221f9b17526d451590664b0e37315d885720f0
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2023-10-21 19:15:42 +0530

    SelectionSort.cpp
    Selection Sort -> selection sort is a simple sorting algorithm in which we traverse each element of the array and put smallest of all at the starting of unsorted array.
    Since for finding the smallest element, we compare each array element (O(n)) with every other array element(O(n)), 
    It takes O(n*n) = O(n^2) time.
    It requires only O (1) space to sort the array.
    With : )

---
+++ Project/Cpp Progs/sorting/SelectionSort.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+class Solution
+{
+    public:
+    void selectionSort(int arr[], int n)
+    {
+       for(int i=0;i<n-1;i++)
+       {
+           int min=i;
+           for(int j=i+1;j<n;j++)
+            {
+               if(arr[j]<arr[min])
+               {
+                   min = j;
+               }
+            }
+            if(min != i)
+                swap(arr[i],arr[min]);
+       }
+    }
+};
+
+//{ Driver Code Starts.
+ 
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int arr[1000],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  //object of solution class
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
+
+// } Driver Code Ends
